d94a6f76412d290aae1a47013f9f6fa244662dbc
Ryan <73213079+CyanPhoenix51@users.noreply.github.com>
2023-07-31 21:45:18 -0700

    removing Logo drawing code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import './index.scss'
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo';
 import Loader from 'react-loaders';
 
 const Home = () => {
@@ -37,7 +36,6 @@ const Home = () => {
                     <h2>Computer Science Graduate</h2>
                     <Link to="/contact" className='flat-button'>CONTACT ME</Link>
                 </div>
-                {/* <Logo /> */}
 
             </div>
             <Loader type="pacman" />
