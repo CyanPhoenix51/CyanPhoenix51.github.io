8bd469774598ccc0650f18715f7cb988977cdb9c
Ryan <73213079+CyanPhoenix51@users.noreply.github.com>
2023-07-31 21:31:14 -0700

    initial upload

---
+++ src/components/About/index.js
@@ -0,0 +1,56 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngular, faCss3, faGit, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass} strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15} />
+                    </h1>
+                    <p>Hi! I'm' a recent Computer Science graduate, thank you for checking out this site.</p>
+                    <p>I'm currently looking for my first job in the industry. I hope to learn and contribute to the company that hires me. This site showcases my talents that showing that I am ambitious and hard working</p>
+                    <p>Ever since I was a kid programming has been a fascination for me. Seeing how much it is science and art in a perfect balance. I can do anything I set my mind to with this power. I have learned so much, and over the years hope to learn so much more. </p>
+                </div>
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGit} color="#EC4D28" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default About
